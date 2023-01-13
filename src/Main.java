e892a7d09fcc1eb80b9df7a38c9431eb18dccf97
Kimtai <kymdennis3@gmail.com>
2023-01-13 07:30:56 +0300

    display the first list using groceryList.get()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
+
+        ArrayList<String> produceList = new ArrayList<>();
+        produceList.add("nyanya");
+        produceList.add("kitungu");
+        produceList.add("hoho");
+
+
+        ArrayList<String> drinksList = new ArrayList<>();
+        drinksList.add("soda");
+        drinksList.add("beer");
+
+        ArrayList<String> bakeryList = new ArrayList<>();
+        bakeryList.add("bread");
+        bakeryList.add("queen cakes");
+        bakeryList.add("donuts");
+
+        groceryList.add(produceList);
+        groceryList.add(drinksList);
+        groceryList.add(bakeryList);
+
+        System.out.println(groceryList.get(0));
     }
 }
